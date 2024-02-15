eb8701e37346b68eb8b3fde084a6863dbd0dea89
Maria <mariavarava176@gmail.com>
2024-02-15 21:49:46 +0200

    Login and registration features added

--- backend/index.js
+++ backend/index.js
@@ -22,6 +22,49 @@ const app = express();
 
 app.use(express.json());
 
+app.post('/auth/login', registerValidation, async (req, res) => {
+  try {
+    const user = await UserModel.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(404).json({
+        message: 'User not found',
+      });
+    }
+
+    const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+
+    if (!isValidPass) {
+      return res.status(400).json({
+        message: 'Invalid login or password',
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      'secret44',
+      {
+        expiresIn: '30d',
+      },
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: 'Failed to log in',
+    });
+  }
+});
+
 app.post('/auth/register', registerValidation, async (req, res) => {
   try {
     const errors = validationResult(req);

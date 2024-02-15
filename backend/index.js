f359399ca0098f7d2151f7a84acce73614953e86
Maria <mariavarava176@gmail.com>
2024-02-15 20:28:43 +0200

    Creating user in db and encrypting the password using bcrypt

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 import { registerValidation } from './validations/auth.js';
 import UserModel from './models/User.js';
@@ -27,9 +28,20 @@ app.post('/auth/login', registerValidation, async (req, res) => {
     return res.status(400).json(errors.array());
   }
 
-  res.json({
-    success: true,
+  const password = req.body.password;
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const doc = new UserModel({
+    email: req.body.email,
+    fullName: req.body.fullName,
+    avatarUrl: req.body.avatarUrl,
+    passwordHash,
   });
+
+  const user = await doc.save();
+
+  res.json(user);
 });
 
 app.listen(4444, (err) => {

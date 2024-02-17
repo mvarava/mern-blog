c4db9f1d3f93f9145c20896c7d551497f0f176b5
Maria <mariavarava176@gmail.com>
2024-02-17 17:32:50 +0200

    Fetching full post info from backend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -39,7 +39,7 @@ export const Home = () => {
                 key={index}
                 id={post._id}
                 title={post.title}
-                imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
+                imageUrl={post.imageUrl}
                 user={post.user}
                 createdAt={post.createdAt}
                 viewsCount={post.viewsCount}

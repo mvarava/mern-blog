55cf417b6d19987853f234f0e0f4a63fa5128d98
Maria <mariavarava176@gmail.com>
2024-02-18 16:59:04 +0200

    Implemented deleting post feature

--- frontend/src/redux/slices/posts.js
+++ frontend/src/redux/slices/posts.js
@@ -13,6 +13,11 @@ export const fetchTags = createAsyncThunk('posts/fetchTags', async () => {
   return data;
 });
 
+export const fetchRemovePost = createAsyncThunk(
+  'posts/fetchRemovePost',
+  async (id) => await axios.delete(`/posts/${id}`),
+);
+
 const initialState = {
   posts: {
     items: [],
@@ -53,7 +58,18 @@ const postsSlice = createSlice({
       .addCase(fetchTags.rejected, (state) => {
         state.tags.status = 'error';
         state.tags.items = [];
+      })
+      .addCase(fetchRemovePost.pending, (state, action) => {
+        state.posts.items = state.posts.items.filter((post) => post._id !== action.meta.arg);
       });
+    // .addCase(fetchRemovePost.fulfilled, (state, action) => {
+    //   state.tags.status = 'loaded';
+    //   state.tags.items = action.payload;
+    // })
+    // .addCase(fetchRemovePost.rejected, (state) => {
+    //   state.tags.status = 'error';
+    //   state.tags.items = [];
+    // });
   },
 });
 
